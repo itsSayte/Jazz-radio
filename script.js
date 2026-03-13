df008a9b1ef001f2f5cf39403407fe5228d55c53
Sayte <112155083+itsSayte@users.noreply.github.com>
2026-03-13 08:54:16 +0100

    Upgrade architecture

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('change-video-btn').addEventListener('click', function() {
-    var newVideoId = prompt("4xDzrJKXOOY");
-    if (newVideoId) {
-      var iframe = document.getElementById('youtube-video');
-      iframe.src = 'https://www.youtube.com/embed/' + newVideoId;
-    }
-  });
-  
